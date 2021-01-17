47feb48388a6e3f64b2b367a809a9a82a49d90fc
krishna.gavara <krishna.gavara@athenagt.com>
2021-01-17 16:20:28 +0530

    error handled

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -183,7 +183,7 @@ class UserController extends Controller
         }
         else {
 
-            return Response::json(['error'=>'false','message'=>'Invalid Credentials'], 401);
+            return Response::json(['error'=>'false','message'=>'Invalid Credentials'], 400);
         }
     }
 
