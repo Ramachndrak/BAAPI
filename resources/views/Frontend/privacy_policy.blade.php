027ae39bb3a7d11e7eed7c41bba95121c7be896c
krishnaathena <krishna.gavara@athenagt.com>
2021-12-28 21:50:11 +0530

    policy page

--- resources/views/Frontend/privacy_policy.blade.php
+++ resources/views/Frontend/privacy_policy.blade.php
@@ -13,9 +13,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-md-12 col-sm-12 col-lg-12">
-				<div class="logo">
-					<img class="img-responsive" src="{{public/images/logo.jpg}}" alt="logo"> 
-				</div>
+				 <div class="logo">
+                        <img style="margin:auto;display:block" class="img-responsive" src="{{url('public/images/logo.JPG')}}" alt="logo">
+                </div>
 				<p>This privacy policy is an electronic record in the form of an electronic
 contract formed under the Information Technology Act, 2000 and the
 rules made thereunder and the amended provisions pertaining to
@@ -33,6 +33,53 @@ all the terms and conditions of this Privacy Policy, do not use the
 Service. Our privacy policy is subject to change at any time without
 notice. To make sure you are aware of any changes, please review this
 policy periodically.</p>
+
+<h1>Collection of Information</h1>
+<h3>Information provided by you</h3>
+<p>This Privacy Policy applies only to information we collect via the App
+bandhaluanubhandalu (“App”), email, telephone, electronic devices and
+other interactive services. In order to help you get the most out of our
+service, we collect User submitted personally identifiable information
+(“PII”) when you use the Service. For example we may collect
+information you provide when you create an account, create or edit your
+profile, upload content (photographs, essays, lessons, etc.), provide
+profile descriptions or comments, participate in a contest or promotion,
+participate in interactive features of the Service, connect or
+communicate with us via third party social media sites, send us an email,
+request customer support or otherwise communicate with us or
+communicate with others users through the Service. Such information
+may include your name, address, password, email address, and phone
+number for communication purposes. Information may be submitted to
+the App or via telephone or email when communicating with a
+bandhaluanubhandalu.com representative. For students and parents, we
+may collect information related to your payment for tutoring sessions and
+content you upload for the purposes of transmitting it to another user or
+for public display on the App. For tutors, we may collect your country
+specific identification number (SSN, Aadhar etc.) and date of birth, for
+tax purposes or to complete a background check; we also collect content
+you upload for purposes related to transmission to other users or public
+display on the App. As detailed in the “Data Transmission and Storage”
+section below, we use Secure Socket Layer (SSL) encryption on any
+page of the App where we ask you to submit personally identifiable
+information and encrypt all such information for safe storage in our
+system.</p>
+<h3>Information we collect automatically</h3>
+<p>We also gather non-PII when you use the Service, including type of
+browser you use, access time, pages viewed, your IP address; and we
+collect information about the computer or mobile device you use to
+access the Service, including without limitation operating system and
+unique device identifiers and mobile network information. This data is
+used to better understand and enhance the user experience on the
+Apps. bandhaluanubhandalu.com may publish these statistics or share
+them with third parties without including PII. Some browsers allow you to
+indicate that you would not like your online activities tracked, using “Do
+Not Track” indicators (“DNT Indicators”), however we are not obligated
+to respond to these indicators. Presently we are not set up to respond to
+DNT Indicators. This means that we may use latent information about
+your online activities to improve your use of our Service, but such usage
+is consistent with the provisions of this Privacy Policy.</p>
+
+<h3>Information we collect from other sources</h3>
 			</div>
 		</div>
 	</div>
